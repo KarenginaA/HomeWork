516a0340c6042743dc2514591156c09a9772d3cd
KarenginaA <kroup15@mail.ru>
2023-03-24 16:53:32 +0300

    добавила домашку

--- work1/Program.cs
+++ work1/Program.cs
@@ -1,1 +1,71 @@
-﻿
+﻿// Задание 1. Напишите программу, которая на вход принимает два числа и выдает ,какое число большее, а какое меньшее.
+
+//Console.Write("Введите первое число");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Введите второе число");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//if (num1 > num2)
+//{
+ // Console.Write("Число" + num1 + "больше числа" + num2); 
+//}
+//else
+//{
+    //Console.WriteLine($"Число {num2} больше числа {num1}");
+//}
+
+ //Задание 2. Напишите программу,которая на вход принимает три числа и выдает максимальное из этих чисел.
+
+//Console.WriteLine("Введите первое число:");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Введите второе число:");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Введите третье число:");
+//int num3 =  Convert.ToInt32(Console.ReadLine());
+
+//if (num1 > num2 && num1 > num3)
+   //Console.WriteLine(num1);
+//else if (num2 > num1 && num2 > num3) 
+   // Console.WriteLine(num2);
+//else if (num3 > num1 && num3 > num2)    
+   // Console.WriteLine(num3);
+
+   //Задание 3. Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+//Console.Write("Введите число:");
+//int num = Convert.ToInt32(Console.ReadLine());
+//if (num %2 == 0)
+
+//{
+     //Console.WriteLine($"Число {num} четное");
+
+//}
+//else
+//{
+   // Console.WriteLine($"Число {num} нечетное");
+
+//}
+
+// Задание 4. Напишите программу,которая на вход принимает число N, а на выходе показывает все четные числа от 1 до 
+/*
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num%2==0)
+{
+   Console.WriteLine($"Число {num} четное");
+}
+
+int current = 1;
+while(current <= num )
+{
+if (current%2==0)
+{
+    Console.WriteLine(current + "");
+}
+ current++;
+}
+
+
